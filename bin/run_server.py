93b197259c3cf1ed0a35c3b45c9a1871e9137108
woodser <woodser@protonmail.com>
2020-04-12 15:17:36 -0400

    disable ssl by default

--- bin/run_server.py
+++ bin/run_server.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 import BaseHTTPServer, SimpleHTTPServer
-import ssl
+#import ssl
  
 port=9100
 print "Running on port %d" % port
  
 SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map['.wasm'] =    'application/wasm' 
 httpd = BaseHTTPServer.HTTPServer(('localhost', port), SimpleHTTPServer.SimpleHTTPRequestHandler)
-httpd.socket = ssl.wrap_socket(httpd.socket, keyfile="../certs/localhost-key.pem", certfile='../certs/localhost-cert.pem', server_side=True)
+#httpd.socket = ssl.wrap_socket(httpd.socket, keyfile="../certs/localhost-key.pem", certfile='../certs/localhost-cert.pem', server_side=True)
  
 httpd.serve_forever()
