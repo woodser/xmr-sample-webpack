0c9fac6ceb03e3a5da539894a95c05fc85224c35
woodser <13068859+woodser@users.noreply.github.com>
2026-06-05 13:25:24 -0400

    update sample app to use mainnet

--- src/sample_code.ts
+++ src/sample_code.ts
@@ -10,32 +10,36 @@ import moneroTs from "monero-ts";
 runMain();
 async function runMain() {
   
-  // connect to a daemon
-  console.log("Connecting to daemon");
-  let daemon = await moneroTs.connectToDaemonRpc("http://localhost:28081");
-  let height = await daemon.getHeight();            // 1523651
-  let feeEstimate = await daemon.getFeeEstimate();  // 1014313512
-  let txsInPool = await daemon.getTxPool();         // get transactions in the pool
+  console.log("Using monero-ts version: " + moneroTs.getVersion());
+
+  // connect to mainnet daemon without worker proxy
+  let daemon1 = await moneroTs.connectToDaemonRpc({server: "http://xmr-node.cakewallet.com:18081", proxyToWorker: false});
+  console.log("Daemon height 1: " + await daemon1.getHeight());
+
+  // connect to mainnet daemon with worker proxy
+  let daemon2 = await moneroTs.connectToDaemonRpc({server: "http://xmr-node.cakewallet.com:18081", proxyToWorker: true});
+  let height = await daemon2.getHeight();            // 1523651
+  console.log("Daemon height 2: " + height );
+  let feeEstimate = await daemon2.getFeeEstimate();  // 1014313512
+  let txsInPool = await daemon2.getTxPool();         // get transactions in the pool
   
   // create wallet from seed phrase using WebAssembly bindings to monero-project
   console.log("Creating wallet from seed phrase");
   let walletFull = await moneroTs.createWalletFull({
     password: "supersecretpassword123",
-    networkType: moneroTs.MoneroNetworkType.TESTNET,
-    seed: "silk mocked cucumber lettuce hope adrenalin aching lush roles fuel revamp baptism wrist long tender teardrop midst pastry pigment equip frying inbound pinched ravine frying",
-    restoreHeight: 171,
+    networkType: moneroTs.MoneroNetworkType.MAINNET,
+    seed: "fruit utensils auburn nabbing huts hexagon espionage fainted oxygen tattoo azure dash phase opened rotate owner grunt happens usage velvet rhythm deepest utensils velvet rotate",
     server: {
-      uri: "http://localhost:28081",
-      username: "superuser",
-      password: "abctesting123"
-    }
+      uri: "http://xmr-node.cakewallet.com:18081",
+    },
+    restoreHeight: height - 1000
   });
   
   // synchronize with progress notifications
   console.log("Synchronizing wallet");
   await walletFull.sync(new class extends moneroTs.MoneroWalletListener {
     async onSyncProgress(height: number, startHeight: number, endHeight: number, percentDone: number, message: string) {
-      // feed a progress bar?
+      //console.log("Sync progress: " + percentDone + "%");
     }
   });
   
@@ -51,33 +55,8 @@ async function runMain() {
       fundsReceived = true;
     }
   });
-
-  // open wallet on monero-wallet-rpc
-  console.log("Opening monero-wallet-rpc");
-  let walletRpc = await moneroTs.connectToWalletRpc("http://localhost:28084", "rpc_user", "abc123");
-  await walletRpc.openWallet("test_wallet_1", "supersecretpassword123");
-  let primaryAddress = await walletRpc.getPrimaryAddress(); // 555zgduFhmKd2o8rPUz...
-  let balance = await walletRpc.getBalance();               // 533648366742
-  let txs = await walletRpc.getTxs();                       // get transactions containing transfers to/from the wallet
-
-  // send funds from RPC wallet to WebAssembly wallet
-  console.log("Transferring funds from monero-wallet-rpc");
-  let createdTx = await walletRpc.createTx({
-    accountIndex: 0,
-    address: await walletFull.getAddress(1, 0),
-    amount: 5000000n, // amount to transfer in atomic units
-    relay: false // create transaction and relay to the network if true
-  });
-  let fee = createdTx.getFee(); // "Are you sure you want to send... ?"
-  await walletRpc.relayTx(createdTx); // relay the transaction
-  
-  // recipient receives unconfirmed funds within 5s seconds
-  await new Promise(function(resolve) { setTimeout(resolve, 5000); });
-  assert(fundsReceived);
-  
   // close wallets
   console.log("Closing wallets");
   await walletFull.close();
-  await walletRpc.close();
   console.log("Done running XMR sample app");
 }
